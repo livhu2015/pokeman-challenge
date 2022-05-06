fa9543e5755827ad965158a1db019dcadd3eb56c
Livhu <livhuwanimatsigila@gmail.com>
2022-05-06 12:11:14 +0200

    adding some unit test

--- server/docker_run.sh
+++ server/docker_run.sh
@@ -1,5 +1,14 @@
-docker run -p 3002:3000 \
+docker stop pokemon-server
+docker rm pokemon-server
+
+docker build -t pokemon-server-ecr-repo ./server/.
+
+docker run -p 8001:3000 \
 -e PORT=3000 \
 -e POKEMON_URL=https://pokeapi.co/api/v2/pokemon \
 -e POKEMON_FRONT_IMAGE_PREFIX=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon \
+--name pokemon-server \
+-d \
 pokemon-server-ecr-repo
+
+
